c0b04cc3baf4dfb501deacd9374c633c4f3fe99b
Emilia ☆ <emilia.mena26@gmail.com>
2019-08-17 13:30:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,6 @@ def long_planeteer_calls(calls_long)
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.include?(cheese_types)
 end
