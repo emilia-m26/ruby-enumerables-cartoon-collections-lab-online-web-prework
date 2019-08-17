810a6a0691717fc6414a4afc65ed49d88162baf2
Emilia ☆ <emilia.mena26@gmail.com>
2019-08-17 13:26:43 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,13 +1,15 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |item, index| 
+    puts "#{index+1}.#{item}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map {|planeteer| planeteer.capitalize + '!'}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls_long)
+   calls_long.any? {|calls_long| calls_long.length >4}
 end
 
 def find_the_cheese# code an argument here
